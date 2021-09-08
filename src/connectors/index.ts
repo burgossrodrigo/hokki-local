9a79b3305864852c09b46919dd932a340c93d055
burgossrodrigo <rodrigoburgos@live.com>
2021-09-08 02:39:24 -0300

    progress where done

--- src/connectors/index.ts
+++ src/connectors/index.ts
@@ -1,60 +1,85 @@
-import { Web3Provider } from '@ethersproject/providers'
+import { BscConnector } from '@binance-chain/bsc-connector'
+import { ChainId } from '@sushiswap/sdk'
+import { FortmaticConnector } from '../entities/FortmaticConnector'
 import { InjectedConnector } from '@web3-react/injected-connector'
+import { NetworkConnector } from '../entities/NetworkConnector'
+import { PortisConnector } from '@web3-react/portis-connector'
+import { TorusConnector } from '@web3-react/torus-connector'
 import { WalletConnectConnector } from '@web3-react/walletconnect-connector'
 import { WalletLinkConnector } from '@web3-react/walletlink-connector'
-import { PortisConnector } from '@web3-react/portis-connector'
+import { Web3Provider } from '@ethersproject/providers'
 
-import { FortmaticConnector } from './Fortmatic'
-import { NetworkConnector } from './NetworkConnector'
-import UNISWAP_LOGO_URL from '../assets/svg/logo.svg'
+const RPC = {
+  [ChainId.MAINNET]: 'https://eth-mainnet.alchemyapi.io/v2/q1gSNoSMEzJms47Qn93f9-9Xg5clkmEC',
+  [ChainId.ROPSTEN]: 'https://eth-ropsten.alchemyapi.io/v2/cidKix2Xr-snU3f6f6Zjq_rYdalKKHmW',
+  [ChainId.RINKEBY]: 'https://eth-rinkeby.alchemyapi.io/v2/XVLwDlhGP6ApBXFz_lfv0aZ6VmurWhYD',
+  [ChainId.GÖRLI]: 'https://eth-goerli.alchemyapi.io/v2/Dkk5d02QjttYEoGmhZnJG37rKt8Yl3Im',
+  [ChainId.KOVAN]: 'https://eth-kovan.alchemyapi.io/v2/6OVAa_B_rypWWl9HqtiYK26IRxXiYqER',
+  [ChainId.BSC]: 'https://bsc-dataseed.binance.org/',
+  [ChainId.BSC_TESTNET]: 'https://data-seed-prebsc-2-s3.binance.org:8545',
+}
 
-const NETWORK_URL = process.env.REACT_APP_NETWORK_URL
-const FORMATIC_KEY = process.env.REACT_APP_FORTMATIC_KEY
-const PORTIS_ID = process.env.REACT_APP_PORTIS_ID
-const WALLETCONNECT_BRIDGE_URL = process.env.REACT_APP_WALLETCONNECT_BRIDGE_URL
-
-export const NETWORK_CHAIN_ID: number = parseInt(process.env.REACT_APP_CHAIN_ID ?? '1')
-
-if (typeof NETWORK_URL === 'undefined') {
-  throw new Error(`REACT_APP_NETWORK_URL must be a defined environment variable`)
+export function getNetwork(defaultChainId, urls = RPC) {
+  return new NetworkConnector({
+    defaultChainId,
+    urls,
+  })
 }
 
 export const network = new NetworkConnector({
-  urls: { [NETWORK_CHAIN_ID]: NETWORK_URL }
+  defaultChainId: 1,
+  urls: RPC,
 })
 
 let networkLibrary: Web3Provider | undefined
+
 export function getNetworkLibrary(): Web3Provider {
   return (networkLibrary = networkLibrary ?? new Web3Provider(network.provider as any))
 }
 
 export const injected = new InjectedConnector({
-  supportedChainIds: [1, 3, 4, 5, 42]
+  supportedChainIds: [
+    1, // mainnet
+    3, // ropsten
+    4, // rinkeby
+    5, // goreli
+    42, // kovan
+    56, // binance smart chain
+    97, // binance smart chain testnet
+  ],
 })
 
 // mainnet only
 export const walletconnect = new WalletConnectConnector({
-  rpc: { 1: NETWORK_URL },
-  bridge: WALLETCONNECT_BRIDGE_URL,
+  rpc: RPC,
+  bridge: 'https://bridge.walletconnect.org',
   qrcode: true,
-  pollingInterval: 15000
+  pollingInterval: 15000,
 })
 
 // mainnet only
 export const fortmatic = new FortmaticConnector({
-  apiKey: FORMATIC_KEY ?? '',
-  chainId: 1
+  apiKey: process.env.NEXT_PUBLIC_FORTMATIC_API_KEY ?? '',
+  chainId: 1,
 })
 
 // mainnet only
 export const portis = new PortisConnector({
-  dAppId: PORTIS_ID ?? '',
-  networks: [1]
+  dAppId: process.env.NEXT_PUBLIC_PORTIS_ID ?? '',
+  networks: [1],
 })
 
 // mainnet only
 export const walletlink = new WalletLinkConnector({
-  url: NETWORK_URL,
-  appName: 'Uniswap',
-  appLogoUrl: UNISWAP_LOGO_URL
+  url: RPC[ChainId.MAINNET],
+  appName: 'SushiSwap',
+  appLogoUrl: 'https://raw.githubusercontent.com/sushiswap/art/master/sushi/logo-256x256.png',
 })
+
+// mainnet only
+export const torus = new TorusConnector({
+  chainId: 1,
+})
+
+// binance only
+export const binance = new BscConnector({ supportedChainIds: [56] })
--- src/hooks/useFetchListCallback.ts
+++ src/hooks/useFetchListCallback.ts
@@ -1,14 +1,14 @@
-import { nanoid } from '@reduxjs/toolkit'
-import { ChainId } from '@uniswap/sdk'
+import { AppDispatch } from '../state'
+import { ChainId } from '@sushiswap/sdk'
 import { TokenList } from '@uniswap/token-lists'
+import { fetchTokenList } from '../state/lists/actions'
+import { getNetworkLibrary } from '../functions/getNetworkLibrary'
+import { getTokenList } from '../functions/list'
+import { nanoid } from '@reduxjs/toolkit'
+import { resolveENSContentHash } from '../functions/ens'
+import { useActiveWeb3React } from './useActiveWeb3React'
 import { useCallback } from 'react'
 import { useDispatch } from 'react-redux'
-import { getNetworkLibrary, NETWORK_CHAIN_ID } from '../connectors'
-import { AppDispatch } from '../state'
-import { fetchTokenList } from '../state/lists/actions'
-import getTokenList from '../utils/getTokenList'
-import resolveENSContentHash from '../utils/resolveENSContentHash'
-import { useActiveWeb3React } from './index'
 
 export function useFetchListCallback(): (listUrl: string, sendDispatch?: boolean) => Promise<TokenList> {
   const { chainId, library } = useActiveWeb3React()
@@ -17,7 +17,7 @@ export function useFetchListCallback(): (listUrl: string, sendDispatch?: boolean
   const ensResolver = useCallback(
     (ensName: string) => {
       if (!library || chainId !== ChainId.MAINNET) {
-        if (NETWORK_CHAIN_ID === ChainId.MAINNET) {
+        if (chainId === ChainId.MAINNET) {
           const networkLibrary = getNetworkLibrary()
           if (networkLibrary) {
             return resolveENSContentHash(ensName, networkLibrary)
@@ -36,13 +36,20 @@ export function useFetchListCallback(): (listUrl: string, sendDispatch?: boolean
       const requestId = nanoid()
       sendDispatch && dispatch(fetchTokenList.pending({ requestId, url: listUrl }))
       return getTokenList(listUrl, ensResolver)
-        .then(tokenList => {
+        .then((tokenList) => {
           sendDispatch && dispatch(fetchTokenList.fulfilled({ url: listUrl, tokenList, requestId }))
           return tokenList
         })
-        .catch(error => {
+        .catch((error) => {
           console.debug(`Failed to get list at url ${listUrl}`, error)
-          sendDispatch && dispatch(fetchTokenList.rejected({ url: listUrl, requestId, errorMessage: error.message }))
+          sendDispatch &&
+            dispatch(
+              fetchTokenList.rejected({
+                url: listUrl,
+                requestId,
+                errorMessage: error.message,
+              })
+            )
           throw error
         })
     },
